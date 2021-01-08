9d1bff1c151bc6c9a256abf24e8f792c82816318
hayaki821 <khkur821@i.softbank.jp>
2021-01-08 23:05:28 +0900

    create service

--- app/controllers/api/julias_controller.rb
+++ app/controllers/api/julias_controller.rb
@@ -1,21 +1,15 @@
 class Api::JuliasController < ApplicationController
+    require './app/services/create_julia_service'
     before_action :params_type_validate, only: [:create]
   # POST /julias/create
   def create
-    # create julia
-    min_x = julia_params[:min_x]
-    max_x = julia_params[:max_x]
-    min_y = julia_params[:min_y]
-    max_y = julia_params[:max_y]
-    c_real = julia_params[:comp_const][:re] # Complex constant real
-    c_imag = julia_params[:comp_const][:im] # Complex constant imaginary
-
+    # julia params
     count = 100 # boundary condition
-    width = 600
-    height = 600
-    image_data = [] # pixel data
-
-    draw_julia_create(image_data,max_x, min_x, max_y, min_y, width, height, count, c_real, c_imag)
+    width = 500 # canvas width
+    height = 500 # canvas height
+    
+    # create julia
+    image_data = CreateJuliaService.new(julia_params, count, width, height).call # pixel data
  
     render json: {:image_data=>image_data}
   end
@@ -52,56 +46,5 @@ class Api::JuliasController < ApplicationController
     end
   end
 
-  def draw_julia_create(image_data,max_x, min_x, max_y, min_y, width, height, count, c_real, c_imag)
-    height.times do |y|
-        width.times do |x|
-            z_real = ((max_x - min_x) / width) * x + min_x
-            z_imag = ((max_y - min_y) / height) * y + min_y
 
-            set_pixel_color(image_data, width, height, x, y, z_real,z_imag, count, c_real, c_imag)
-        end
-    end
-  end
-
-  def set_pixel_color(image_data, width, height, x, y, z_real,z_imag, count,c_real, c_imag)
-    # Calculation julia recurrence formula
-    count.times do |i|
-         # if Divergence
-        if z_real * z_real + z_imag * z_imag >= 4
-            image_data[(y * width + x) * 4 + 0] = set_rgb(i,"R", count)
-            image_data[(y * width + x) * 4 + 1] = set_rgb(i,"G", count)
-            image_data[(y * width + x) * 4 + 2] = set_rgb(i,"B", count)
-            image_data[(y * width + x) * 4 + 3] = 255
-            return
-        end
-
-        # update
-        real = z_real * z_real - z_imag * z_imag + c_real
-        imag = 2 * z_real * z_imag + c_imag
-        z_real = real
-        z_imag = imag
-    end
-
-    # convergence
-    image_data[(y * width + x) * 4 + 0] = 0
-    image_data[(y * width + x) * 4 + 1] = 0
-    image_data[(y * width + x) * 4 + 2] = 0
-    image_data[(y * width + x) * 4 + 3] = 255
-  end
-
-  # Calculation color
-  def set_rgb(n, color, count)
-    percent = n / count.to_f
-    # decide color pixels
-    case color
-    when "R" 
-        return percent ** 0.3 * 255
-    when "G" 
-        return 0
-    when "B" 
-        return (1 - percent) ** 0.3 * 255
-    else
-        return 0
-    end    
-  end
 end
